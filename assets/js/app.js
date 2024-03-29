75429edac7be07ca66fcf19dc1cd586a04e2a4e3
Claudio Poblete <pobleteclaudio2103@gmail.com>
2024-03-29 16:05:02 -0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,79 @@
+const dogImg = document.querySelector('#dog-img')
+const paraVerification = document.querySelector('#verification-para')
+const verificationButton = document.querySelector('#verification-btn')
+const passButton = document.querySelector('#pass-btn')
+const paraPass = document.querySelector('#pass-para')
+
+const changeColor = () =>{
+  dogImg.style.border = "2px solid red"
+}
+
+const removeColor = () =>{
+  dogImg.style.border = "none"
+}
+
+let clickCount = 0
+
+dogImg.addEventListener('click', ()=>{
+  clickCount++
+  if(clickCount == 1){
+    changeColor()
+  }else if(clickCount == 2){
+    removeColor()
+    clickCount = 0
+  }
+})
+
+verificationButton.addEventListener('click', () =>{
+  const stickerUno = document.querySelector('#stickerUno').valueAsNumber
+  const stickerDos = document.querySelector('#stickerDos').valueAsNumber
+  const stickerTres = document.querySelector('#stickerTres').valueAsNumber
+  let totalSticker = stickerUno + stickerDos + stickerTres
+  if(totalSticker <= 10 && totalSticker > 0){
+    paraVerification.innerHTML = "Llevas "+ totalSticker +" sticker"
+  } else if (totalSticker > 10){
+    paraVerification.innerHTML = "No puedes llevar más de 10 stickers"
+  } else{
+    paraVerification.innerHTML = "No llevas ningún sticker"
+  }
+ })
+
+let selectUno = document.querySelector('#firstPass')
+let selectDos = document.querySelector('#secondPass')
+let selectTres = document.querySelector('#thirdPass')
+
+for(let opcionUno = 1; opcionUno < 10; opcionUno++){
+  let option = document.createElement('option')
+  option.value = opcionUno
+  option.text = opcionUno
+  selectUno.appendChild(option)
+}
+
+for(let opcionDos = 1; opcionDos < 10; opcionDos++){
+  let option = document.createElement('option')
+  option.value = opcionDos
+  option.text = opcionDos
+  selectDos.appendChild(option)
+}
+
+for(let opcionTres = 1; opcionTres < 10; opcionTres++){
+  let option = document.createElement('option')
+  option.value = opcionTres
+  option.text = opcionTres
+  selectTres.appendChild(option)
+}
+
+passButton.addEventListener('click', () =>{
+  selectUno = document.querySelector('#firstPass')
+  selectDos = document.querySelector('#secondPass')
+  selectTres = document.querySelector('#thirdPass')
+  let password = selectUno.value + selectDos.value + selectTres.value
+  if(password == "911"){
+    paraPass.innerHTML = "La contraseña Nro 1 es correcta"
+  } else if(password == "714"){
+    paraPass.innerHTML = "La contraseña Nro 2 es correcta"
+  } else{
+    paraPass.innerHTML = "La contraseña es incorrecta"
+  }
+})
+
